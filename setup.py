ba5a9dff6229f81baae20a7f38784fa854d1759f
Jaikinator <jaikinator@googlemail.com>
2021-12-04 15:03:58 +0100

    test dependency_links to setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
                         "torch==1.10.0",
                         "dqc == 0.2.0.dev1172031026566"],
     url='https://github.com/Jaikinator/OptBasisSets',
-    dependency_links=["https://github.com/Jaikinator/dqc/tarball/master#egg=0.2.0.dev1172031026566"],  # dqc nightly
+    dependency_links=["dqc = https://github.com/Jaikinator/dqc/tarball/master#egg=0.2.0.dev1172031026566"],  # dqc nightly
     license='',
     author='Jacob ',
     author_email='',
