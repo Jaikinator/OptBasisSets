e650d6f59eb7270b825147dc82d74e959a9be5dc
Jaikinator <jaikinator@googlemail.com>
2021-12-04 12:48:03 +0100

    added requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
 from setuptools import setup
-
+req = ["dqc~=0.2.0.dev1172031026566",
+        "xitorch~=0.3.0",
+        "pyscf~=2.0.1",
+        "ase~=3.22.1",
+        "setuptools~=58.0.4",
+        "numpy~=1.21.2",
+        "ase==3.22.1",
+        "basis-set-exchange==0.9",
+        "torch==1.10.0"],
 setup(
     name='OptBasisSets',
     version='0.1',
     packages=['optb'],
+    requires=req,
     url='https://github.com/Jaikinator/OptBasisSets',
     license='',
     author='Jacob ',
