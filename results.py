68bab7561e7a122b9c89230e63c9c220a257796d
Jaikinator <jaikinator@googlemail.com>
2022-02-03 14:27:05 +0100

    more plot stuff

--- results.py
+++ results.py
@@ -30,17 +30,18 @@ df.drop(indx, inplace=True)
 # plt.legend()
 
 print(df.columns.values)
-# print(df)
-# fig, ax = plt.subplots(1,1)
-# ax.scatter(df.index,df['STO-3G_energy'], label = 'STO-3G_energy')
-# ax.scatter(df.index,df["cc-pvtz_energy"], label ="cc-pvtz_energy")
-# ax.scatter(df.index,df['STO-3G_opt_energy'], label ='STO-3G_opt_energy')
-# ax.set_ylabel("energy")
-# ax.set_xticks(df.index)
-# ax.set_xticklabels(df["molecule"], rotation='vertical', fontsize=12)
-# plt.legend()
-#
-# plt.show()
+print(df)
+fig, ax = plt.subplots(1,1)
+ax.scatter(df.index,abs(df['STO-3G_energy']), label = 'STO-3G_energy')
+ax.scatter(df.index,abs(df["cc-pvtz_energy"]), label ="cc-pvtz_energy")
+ax.scatter(df.index,abs(df['STO-3G_opt_energy']), label ='STO-3G_opt_energy')
+ax.set_ylabel("energy")
+ax.set_xticks(df.index)
+ax.set_xticklabels(df["molecule"], rotation='vertical', fontsize=12)
+plt.legend()
+
+
+plt.show()
 
 molec = set(df["molecule"])
 if not os.path.exists("plots"):
