ea27eaba0e1cd6d8f474e2a0aebedc8cf1696179
jaikinator <schmieder.jacobweb.de>
2022-04-08 15:04:37 +0200

    more evaluation stuff

--- results.py
+++ results.py
@@ -6,6 +6,7 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from warnings import warn
 
 pd.set_option("display.max_rows", 999)
 pd.set_option('display.max_columns', 500)
@@ -14,87 +15,123 @@ pd.set_option('display.width', 1000)
 path = "results.csv"
 
 
-df2 = pd.read_csv(path,index_col=0).reset_index(drop=True)
+df = pd.read_csv(path,index_col=0).reset_index(drop=True)
+molec_old = set(df["molecule"])
 
-df2["optb-sto3g"] = df2["STO-3G_opt_energy"] - df2["STO-3G_energy"]
-df2["cc-pvtz-sto3g"] = df2["cc-pvtz_energy"] - df2["STO-3G_energy"]
-df2["improvement in %"] = (100 / df2["STO-3G_energy"] * df2["STO-3G_opt_energy"]) - 100
-molec = set(df2["molecule"])
+
+
+df["optb-initial"] = df["opt_energy"] - df["initial_energy"]
+df["reference-initial"] = df["ref_energy"] - df["initial_energy"]
+df["rel. improvement %"] = 100/ df["reference-initial"] * df["optb-initial"]
+
+indx = df[df["optb-initial"] > 0].index
+df.drop(indx, inplace=True)
+indx2 = df[df["optb-initial"] > -1e-5].index
+df.drop(indx2, inplace=True)
+indx3 = df[df["best_i"] <+ 10].index
+df.drop(indx3, inplace=True)
+indx4 = df[df["rel. improvement %"] > 100].index
+df.drop(indx4, inplace=True)
+indx5 = df[df["rel. improvement %"] < 0].index
+df.drop(indx5, inplace=True)
+
+df.reset_index(drop=True, inplace=True)
+# reorder columns of df
+df = df[['molecule', 'basis', 'ref. basis','learning rate', 'maxiter', 'method', 'f_rtol', 'best_f', 'best_df',
+           'best_i', 'opt_energy', 'initial_energy', 'ref_energy', 'optb-initial',
+           'reference-initial', 'rel. improvement %']]
+
+df.to_csv("evaluation/results_filtered.csv", index=False)
+
+molec = set(df["molecule"])
+
+print("molecule difference: ", len(molec_old - molec),  molec_old - molec)
+
+if not os.path.exists("evaluation"):
+    os.mkdir("evaluation")
+
+if os.path.exists("plots"):
+    os.rename("plots", "evaluation/plots")  # move plots folder to evaluation folder
+
+#remove results_filtert.csv to evaluation folder
+
+
+with open("evaluation/molecules_dropped.txt", "w") as f:
+    f.write("molecule dropped because the results where to bad): "
+            + str(len(molec_old - molec)) + "\n")
+    for i in molec_old - molec:
+        f.write(i + "\n")
+
 improvement = []
 meanimprov = []
 index2arr = []
-indx2 = df2[df2["improvement in %"] < 0].index
-df2.loc[indx2, "improvement in %"] = np.nan
+
 
 for mol in molec:
-    indx = df2[df2["molecule"] == mol].index
-    indx2 = df2[df2["improvement in %"] > 0].index
-    df2.loc[indx2]["improvement in %"] = np.nan
+    indx = df[df["molecule"] == mol].index
+    indx2 = df[df["rel. improvement %"] > 0].index
+    indx2 = indx2.append(df[df["rel. improvement %"] > 100].index)
 
-    # print(indx2)
-    improvement.append([mol, np.max(df2.loc[indx]["improvement in %"]), np.mean(df2.loc[indx]["improvement in %"])])
+    df.loc[indx2]["rel. improvement %"] = np.nan
+
+    improvement.append([mol, np.max(df.loc[indx]["rel. improvement %"]),
+                        np.mean(df.loc[indx]["rel. improvement %"]),
+                        df[df["rel. improvement %"] == np.max(df.loc[indx]["rel. improvement %"])].index.values])
 
 improvement = np.asanyarray(improvement)
 
-df3 = pd.DataFrame()
-df3["mol"] = improvement[:,0]
-df3["best improvement %"] = [float(improvement[i,1]) for i in range(len(improvement[:,1]))]
-df3["mean improvement %"] = [float(improvement[i,2]) for i in range(len(improvement[:,2]))]
-# index_mal_wieder = df3[np.isnan(df3["best improvement %"]) == True].index
-# df3 = df3.fillna(0)
-df3 = df3.sort_values("best improvement %", ignore_index = True)
-print(df3.columns.values, df2.columns.values)
-dfdf = pd.concat([df2,df3], axis= 1)
-
-#print(dfdf)
-dfdf.to_csv("plots/results_new.csv", na_rep='NaN',index=False, )
+df2 = pd.DataFrame()
+df2["mol"] = improvement[:,0]
+df2["best rel. improvement %"] = [float(improvement[i,1]) for i in range(len(improvement[:,1]))]
+df2["mean rel. improvement %"] = [float(improvement[i,2]) for i in range(len(improvement[:,2]))]
+df2["index best rel. improvement %"] = [int(improvement[i,3][0]) for i in range(len(improvement[:,2]))]
 
 
+df_best = pd.DataFrame(df.iloc[[int(improvement[i,3][0]) for i in range(len(improvement[:,2]))]])
+df_best.reset_index(drop=False, inplace=True)
+dfdf= pd.concat([df2, df_best], axis=1)
 
-#
-# df = pd.read_csv(path,index_col=0).dropna().reset_index(drop=True)
-#
-# df["optb-sto3g"] = df["STO-3G_opt_energy"] - df["STO-3G_energy"]
-# df["cc-pvtz-sto3g"] = df["cc-pvtz_energy"] - df["STO-3G_energy"]
-# # ax = df.plot.bar(x='molecule', y='abs. energy diff (sto3g and opt)', rot=0)
-# indx = df[df["optb-sto3g"] > 0].index
-# df.drop(indx, inplace=True)
-# indx2 = df[df["optb-sto3g"] > -1e-3].index
-# df.drop(indx2, inplace=True)
-#
-# #relativ improvement
-#
-# df["improvement in %"] = (100 / df["STO-3G_energy"] * df["STO-3G_opt_energy"]) - 100
-#
-# #best config for each molec
-# molec = set(df["molecule"])
-# improvement = []
-# index2arr = []
-# for mol in molec:
-#     indx = df[df["molecule"] == mol].index
-#     indx2 = df[df["improvement in %"] == max(df.loc[indx]["improvement in %"])].index
-#     index2arr.append(indx2.tolist()[0])
-#     improvement.append([mol, max(df.loc[indx]["improvement in %"])])
-#
-#
-# improvement = np.array(improvement)
-# fig, ax = plt.subplots(1,1)
-#
-# ax.plot(improvement[:,0], improvement[:,1])
-# ax.set_ylabel("improvement in %")
-# # ax.set_xticks([])
-# #ax.set_xticklabels(df.loc[index2arr]["molecule"], rotation='vertical', fontsize=12)
-#
-# # plt.legend()
-# plt.show()
+index_eq = ((dfdf["index best rel. improvement %"] == dfdf["index"])).all()
+
+if index_eq:
+    dfdf.drop(["index best rel. improvement %", "index", "molecule"], axis=1, inplace=True)
+else:
+    warn("index best rel. improvement % and index are not the same")
+
+dfdf.sort_values("best rel. improvement %", ignore_index = True ,inplace= True)
+dfdf.rename(columns={"mol": "molecule"}, inplace=True)
+
+#reorder columns of dfdf
+dfdf = dfdf[['molecule', 'basis', 'ref. basis', 'best rel. improvement %', 'mean rel. improvement %',
+             'opt_energy', 'initial_energy', 'ref_energy', 'optb-initial', 'reference-initial', 'method',
+             'learning rate', 'maxiter', 'f_rtol', 'best_f', 'best_df', 'best_i',]]
+
+dfdf.to_csv("evaluation/best_res.csv", na_rep='NaN',index=False)
+#delete all plots in evaluation/plots folder
+for file in os.listdir("evaluation/plots"):
+    os.remove("evaluation/plots/" + file)
+    print("removed: ", file)
+
+#make a scatter plot using  'best rel. improvement %', 'mean rel. improvement %' and highlight the best results
+for i in range(len(dfdf)):
+    plt.scatter(dfdf.loc[i]["best rel. improvement %"], dfdf.loc[i]["mean rel. improvement %"], label=dfdf.loc[i]["molecule"])
+    #plt.annotate(dfdf.loc[i]["molecule"], xy=(dfdf.loc[i]["best rel. improvement %"], dfdf.loc[i]["mean rel. improvement %"]))
+
+plt.xlabel("best rel. improvement %")
+plt.ylabel("mean rel. improvement %")
+
+plt.show()
+
+
+
 
 
 # print(df.columns.values)
-def create_plots():
+def create_plots_old(df):
     # fig, ax = plt.subplots(1,1)
-    # ax.scatter(df.index,abs(df['STO-3G_energy']), label = 'STO-3G_energy')
-    # ax.scatter(df.index,abs(df["cc-pvtz_energy"]), label ="cc-pvtz_energy")
-    # ax.scatter(df.index,abs(df['STO-3G_opt_energy']), label ='STO-3G_opt_energy')
+    # ax.scatter(df.index,abs(df['initial_energy']), label = 'initial_energy')
+    # ax.scatter(df.index,abs(df["ref_energy"]), label ="ref_energy")
+    # ax.scatter(df.index,abs(df['opt_energy']), label ='opt_energy')
     # ax.set_ylabel("energy")
     # ax.set_xticks(df.index)
     # ax.set_xticklabels(df["molecule"], rotation='vertical', fontsize=12)
@@ -106,9 +143,9 @@ def create_plots():
     for mol in molec:
         indx = df[df["molecule"] == mol].index
         xval = df.loc[indx]["learning rate"]
-        yval_sto3g = df.loc[indx]['STO-3G_energy']
-        yval_sto3g_opt = df.loc[indx]["STO-3G_opt_energy"]
-        yval_cc_pvtz = df.loc[indx]["cc-pvtz_energy"]
+        yval_sto3g = df.loc[indx]['initial_energy']
+        yval_sto3g_opt = df.loc[indx]["opt_energy"]
+        yval_cc_pvtz = df.loc[indx]["ref_energy"]
         f_rtol = df.loc[indx]["f_rtol"]
 
         fig, (ax1, ax2) = plt.subplots(1, 2)
@@ -139,8 +176,7 @@ def create_plots():
                    fancybox=True, shadow=True, ncol=1, prop={'size': 8})
         fig.tight_layout()
 
-        fig.savefig(f"plots/{mol}.png", dpi = 100)
+        fig.savefig(f"plots/{mol}.png", dpi = 500)
         plt.close(fig)
 
         print(f"plots/{mol}.png done")
-
