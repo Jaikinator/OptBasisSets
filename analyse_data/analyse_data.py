bea0f1c5aac7e43449182e88080e74a36b835ced
jaikinator <schmieder.jacobweb.de>
2022-05-13 15:38:16 +0200

    analyse stuff

--- analyse_data/analyse_data.py
+++ analyse_data/analyse_data.py
@@ -6,13 +6,14 @@ import os
 import time
 
 import pandas as pd
+import plotly.graph_objects as go
 import plotly.express as px
 import matplotlib.pyplot as plt
 import numpy as np
 from optb import loadatomstruc
 from optb.data.avdata import elg2 as g2, elw417 as w417
 import warnings
-import logging as log
+
 
 pd.set_option("display.max_rows", 1000)
 pd.set_option('display.max_columns', 500)
@@ -43,6 +44,7 @@ def calculate_impact(df):
 
     # create impact column
     df["ref-opb [hartree /atom]"] = df["ref_energy [hartree/atom]"] - df["optb_energy [hartree /atom]"]
+    df["ref-init [hartree /atom]"] = df["ref_energy [hartree/atom]"] - df["initial_energy [hartree/atom]"]
     df.reset_index(drop=True, inplace=True)
 
     return df
@@ -58,7 +60,7 @@ def drop_small_best_i(df, threshold = 10):
     print(f"\n\tDropped {df_len - len(df)} results.\n")
     return df
 
-def remove_SCF_not_converged(df, threshold = 10):
+def remove_SCF_not_converged(df, threshold = 100):
     """
     Remove the SCF not converged results.
     """
@@ -75,41 +77,87 @@ def remove_SCF_not_converged(df, threshold = 10):
         _df_mol = _df.copy().iloc[ind]
 
         molec_old = set(_df_mol["molecule"])
-
+        # drop result where the reference scf calculation is not converged
         ind_not_converged = _df_mol[abs(_df_mol["ref_energy"] / _df_mol["initial_energy"]) > threshold].index
 
-        if str(basisv) == "('aug-cc-pVDZ', 'aug-pc-2')":
-            print("Hello", basisv)
-            test = _df_mol[_df_mol["molecule"] == "CH3OCH3"]
-            print(test )
-            print(test["ref_energy"] / test["initial_energy"])
-        _df_mol.drop(ind_not_converged, inplace=True) #just to check which molecules are dropped
+        _df_mol.drop(ind_not_converged, inplace=True) # just to check which molecules are dropped
         _df_mol.reset_index(drop=True, inplace=True)
 
-        _df.drop(ind_not_converged, inplace=True) # actual drop the results in the main dataframe
+        _df.drop(ind_not_converged, inplace=True)  # actual drop the results in the main dataframe
         _df.reset_index(drop=True, inplace=True)
 
-        molec_new = set(_df_mol["molecule"])
-        print(f"\n\t For basis variation: {basisv}\n\t Removed {len(molec_old - molec_new)} molecules.")
+        molec_drop_scf_not_conv = set(_df_mol["molecule"])
 
+        # drop results where the optimized basis set is not converged in the scf calculation
 
-        if len(molec_old - molec_new) > 0:
-            print(f"\n\tThe molecules that are not converged for {basisv} are written in evaluation/molecules_dropped_{basisv}.txt\n")
+        ind = _df[_df["basis_variation"] == basisv].index
+        _df_mol_2 = _df.copy().iloc[ind]
+
+        ind_not_converged_optb = _df_mol_2[abs(_df_mol_2["opt_energy"] / _df_mol_2["ref_energy"]) > threshold].index
+
+        _df_mol_2.drop(ind_not_converged_optb, inplace=True) # just to check which molecules are dropped because of optb energy too high
+        _df_mol_2.reset_index(drop=True, inplace=True)
+
+        molec_drop_scf_optb_not_conv = set(_df_mol_2["molecule"])
+
+        _df.drop(ind_not_converged_optb, inplace=True) # actual drop the results in the main dataframe
+        _df.reset_index(drop=True, inplace=True)
+
+        print(f"For basis variation: {basisv}\n\t in total removed {len(molec_old - molec_drop_scf_optb_not_conv)} molecules.")
+        print(f"\t removed because the scf of a given basis wasn't converged "
+              f"{len(molec_old - molec_drop_scf_not_conv)} molecules.")
+        print(f"\t removed because the scf of a optimised basis wasn't converged "
+              f"{len(molec_drop_scf_not_conv - molec_drop_scf_optb_not_conv)} molecules.")
+
+        if len(molec_old - molec_drop_scf_optb_not_conv) > 0:
+            print(f"\n\tThe molecules that are not converged for {basisv} are "
+                  f"written in evaluation/molecules_dropped_{basisv}.txt\n")
             with open(f"evaluation/molecules_dropped_{basisv}.txt", "w") as f:
-                f.write(f"molecule of the basis variation {basisv} dropped because the results of pyscf are not converged for large basis): "
-                        + str(len(molec_old - molec_new)) + "\n")
-                for i in molec_old - molec_new:
+                f.write(f"molecule of the basis variation {basisv} dropped because the results "
+                        f"of pyscf are not converged for large basis): "
+                        + str(len(molec_old - molec_drop_scf_not_conv)) + "\n")
+
+                for i in molec_old - molec_drop_scf_not_conv:
                     f.write(i + "\n")
+
+                f.write(f"molecule of the basis variation {basisv} dropped because the results "
+                        f"of pyscf are not converged for optimised basis set): "
+                        + str(len(molec_drop_scf_not_conv - molec_drop_scf_optb_not_conv)) + "\n")
+
+                for i in molec_drop_scf_not_conv - molec_drop_scf_optb_not_conv:
+                    f.write(i + "\n")
+
     return _df
 
 
-def get_best_res_mol(df):
+def select_db(df,db):
+    """
+    Choose the database to use.
+    :param df: dataframe with the results
+    :param db: str - the name of the database to use.
+    :return: pd.DataFrame - molecule from the choosen db.
+    """
+
+    if db == "g2":
+        _df= df[df['database'] == "g2"]
+        _df.reset_index(drop=True, inplace=True)
+        return _df
+    elif db == "w417":
+        _df = df[df['database'] == "w417"]
+        _df.reset_index(drop=True, inplace=True)
+        return _df
+    else:
+        raise ValueError("The database must be local or remote")
+
+
+def get_best_res_mol(df, db ="w417"):
     """
     Get the best result for each molecule and basis variation.
     """
     _df = df.copy()
     _df = calculate_impact(_df)
-    _df = drop_small_best_i(_df, threshold=10)
+    _df = select_db(_df, db)
+    _df = drop_small_best_i(_df, threshold= 10)
     _df = remove_SCF_not_converged(_df, threshold=10)
     out_df = pd.DataFrame(columns = _df.columns.values)
 
@@ -127,95 +175,72 @@ def get_best_res_mol(df):
 
 def get_average_impact_bv(df):
     mean_imp_arr = []
+    mean_total_arr = []
     basis_var_arr = []
     for var in set(df["basis_variation"]):
         ind_val = df[df["basis_variation"] == var].index
-        if var == "('aug-cc-pVDZ', 'aug-pc-2')":
-            print(df[df["molecule"] == "CH3OCH3" ])
-
         mean_val = df["ref-opb [hartree /atom]"][ind_val].mean()
+        mean_total_avg = df["ref-init [hartree /atom]"][ind_val].mean()
         mean_imp_arr.append(mean_val)
+        mean_total_arr.append(mean_total_avg)
         basis_var_arr.append(var)
 
-    df_mean_basis_var = pd.DataFrame({"basis_variation": basis_var_arr,"mean energy difference": mean_imp_arr})
+    df_mean_basis_var = pd.DataFrame({"basis_variation": basis_var_arr,
+                                      "mean energy difference [hartree/atom]": mean_imp_arr ,
+                                      "ref - init energy difference [hartree/atom]":mean_total_arr})
     df_mean_basis_var["basis_variation"] = df_mean_basis_var["basis_variation"].astype(str)
 
     return df_mean_basis_var
 
-def filter_good_results(df):
-    """
-    Filter the good results.
-    """
-    _df = df.copy()
-
-    _df = drop_small_best_i(_df)
-
-    indx = _df[_df["optb-initial"] > 0].index
-    _df.drop(indx, inplace=True)
-
-    indx3 = _df[_df["best_i"] < + 10].index
-    _df.drop(indx3, inplace=True)
-    indx4 = _df[_df["rel. improvement %"] > 100].index
-    _df.drop(indx4, inplace=True)
-    indx5 = _df[_df["rel. improvement %"] < 0].index
-    _df.drop(indx5, inplace=True)
-
-    return _df
-
-def filter_bad_results(df):
-    """
-    Filter the bad results.
-    """
-
-    _df = df.copy()
 
 
-    indx = _df[_df["optb-initial"] < 120].index
-    _df.drop(indx, inplace=True)
-    # indx2 = df[df["optb-initial"] < 1e-5].index
-    # df.drop(indx2, inplace=True)
-    indx4 = _df[_df["rel. improvement %"] < 120].index
-    _df.drop(indx4, inplace=True)
-    # indx5 = df[df["rel. improvement %"] > 0].index
-    # df.drop(indx5, inplace=True)
-    _df.reset_index(drop=True, inplace=True)
-    return _df
-
-
-path = "/nfs/data-013/jaikinator/PycharmProjects/OptBasisSets/results.csv"
-df = pd.read_csv(path,index_col=0).reset_index(drop=True)
-
-# df = calculate_impact(df)
-# df = drop_small_best_i(df, threshold=10)
-
-df_best = get_best_res_mol(df)
-df_best.to_csv("evaluation/best_results.csv", index=False)
-
-
-
-# path = "/nfs/data-013/jaikinator/PycharmProjects/OptBasisSets/analyse_data/evaluation/best_results.csv"
+# path = "/nfs/data-013/jaikinator/PycharmProjects/OptBasisSets/results.csv"
 # df = pd.read_csv(path,index_col=0).reset_index(drop=True)
-# df = get_average_impact_bv(df)
-# print(df)
+#
+# df_best = get_best_res_mol(df, db = "w417")
+# df_best.to_csv("evaluation/best_results_w417.csv", index=False)
 
-#
-# mean_imp_arr = []
-# basis_var_arr = []
-#
-# for var in basis_var:
-#     ind_val = df[df["basis variation"] == var].index
-#     mean_val = df["improvement %"][ind_val].mean()
-#     mean_imp_arr.append(mean_val)
-#     basis_var_arr.append(var)
-#
-# df_mean_basis_var = pd.DataFrame({"basis variation": basis_var_arr,"mean improvement %": mean_imp_arr})
-# df_mean_basis_var["basis variation"] = df_mean_basis_var["basis variation"].astype(str)
-# df_mean_basis_var.to_csv("evaluation/mean_basis_var.csv")
-#
-# print(df_mean_basis_var)
-#
-# fig = px.bar(df_mean_basis_var, x="basis variation", y="mean improvement %",
-#              title="Mean improvement per basis variation")
-# fig.update_layout(font=dict(size=20))
-#
-# fig.write_html("evaluation/mean_basis_var_not_filtert.html")
+
+
+path = "/nfs/data-013/jaikinator/PycharmProjects/OptBasisSets/analyse_data/evaluation/best_results_w417.csv"
+df = pd.read_csv(path,index_col=0).reset_index(drop=True)
+df = get_average_impact_bv(df)
+df["mean energy difference [hartree/atom]"] = df["mean energy difference [hartree/atom]"].abs()
+df["ref - init energy difference [hartree/atom]"] = df["ref - init energy difference [hartree/atom]"].abs()
+
+#plotly bar plot with two y axis
+
+
+
+# fig = px.bar(df, x="basis_variation", y="mean energy difference [hartree/atom]",color="ref - init energy difference [hartree/atom]",barmode='group',
+#              title="Average impact of basis variation")
+# fig.update_layout(
+#     #xaxis_showticklabels=False,
+#     title_text="Average impact of basis variation",
+#     xaxis_title="Basis variation",
+#     yaxis_title="Average energy difference [Hartree/atom]",
+#     font=dict(
+#         family="Courier New, monospace",
+#         size=18,
+#         color="#000066"
+#     )
+# )
+
+print(df)
+
+fig = go.Figure(data=[
+    go.Bar(name='mean energy difference between the reference and the optimised basis', x=df["basis_variation"], y=df["mean energy difference [hartree/atom]"]),
+    go.Bar(name='mean energy difference between two basis sets', x=df["basis_variation"], y=df["ref - init energy difference [hartree/atom]"])
+])
+# Change the bar mode
+fig.update_layout(barmode='group',
+                  title_text="Average impact of optimisation",
+                  xaxis_title="Basis variation",
+                  yaxis_title="Energy difference [Hartree/atom]",
+                  font=dict(
+                      family="Courier New, monospace",
+                      size=18,
+                      color="#000066"
+                  )
+                  )
+fig.write_html("evaluation/avg_energy_bv.html")
