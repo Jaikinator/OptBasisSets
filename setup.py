dc36f1742c574d45035de20e205396a4665ea467
jaikinator <schmieder.jacobweb.de>
2022-05-17 14:29:57 +0200

    added support versioning and updated version

--- setup.py
+++ setup.py
@@ -1,9 +1,40 @@
-from setuptools import setup
+import re
+import os
+from setuptools import setup, find_packages
+
+module_name = "optb"
+github_url = "https://github.com/Jaikinator/OptBasisSets.git"
+
+# open readme and convert all relative path to absolute path
+#descriptioninf for the module
+# with open("README.md", "r") as f:
+#     long_desc = f.read()
+#
+# link_pattern = re.compile(r"\(([\w\-/]+)\)")
+#
+# link_repl = r"(%s\1)" % github_url
+# long_desc = re.sub(link_pattern, link_repl, long_desc)
+
+
+file_dir = os.path.dirname(os.path.realpath(__file__))
+absdir = lambda p: os.path.join(file_dir, p)
+
+############### versioning ###############
+verfile = os.path.abspath(os.path.join(module_name, "version.py"))
+version = {"__file__": verfile}
+
+with open(verfile, "r") as fp:
+    exec(fp.read(), version)
+
+############### setup ###############
+
+build_version = "OPTB_BUILD" in os.environ
 
 setup(
-    name='optb',
-    version='0.1.3.0',
-    packages=['optb', 'optb/data'],
+    name=module_name,
+    version=version["get_version"](build_version),
+    packages=find_packages(),
+    python_requires="~=3.9",
     install_requires = ["pyscf~=2.0.1",
                         "ase~=3.22.1",
                         "setuptools~=58.0.4",
@@ -18,7 +49,7 @@ setup(
                         "knockknock>=0.1.8.1"], # fork of telegram message
     url='https://github.com/Jaikinator/OptBasisSets',
     license='',
-    author='Jacob ',
+    author='Jaikinator',
     author_email='',
-    description='setup pyscf and dqc to optimize gaussian basis '
+    description='optimize gaussian basis sets'
 )
