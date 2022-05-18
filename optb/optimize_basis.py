ac266093c027f528b203fead2cee849fca02102b
jaikinator <schmieder.jacobweb.de>
2022-05-18 13:25:15 +0200

    added OPTBASIS Class to simplyfy optimization setup

--- optb/optimize_basis.py
+++ optb/optimize_basis.py
@@ -10,6 +10,7 @@ from numpy import any , isnan , nan
 from optb.projection import projection
 from optb.Mole import Mole_minimizer
 from optb.basis_converter import bconv
+from optb.AtomsDB import loadatomstruc
 
 
 def scf_dft_energy(basis :dict , atomstruc : list, atomstrucstr = None, outpath : str = "./"):
@@ -372,6 +373,263 @@ def optimize_basis_old(basis: str,
     else:
        raise "Something went wrong"
 
+
+class OPTBASIS:
+
+    def __init__(self,basis: str,
+                   basis_ref : str,
+                   molecule : Union[str, list],
+                   step:  float,
+                   maxiter : int = 100000000,
+                   miniter : int = 1 ,
+                   method: str = "Adam",
+                   diverge = -1.0 ,
+                   maxdivattempts = 50,
+                   output_path = None,
+                   get_misc = True,
+                   mol_kwargs: dict = {},
+                   minimize_kwargs: dict = {},
+                   out_kwargs : dict = {}):
+        """
+            Class to optimize basis functions. It uses the xitorch.optimize.minimize function.
+            be aware that the basis functions are optimized in the small basis set.
+            :param basis: str basis which should be optimized
+            :param basis_ref: str reference basis
+            :param molecule: Molecule Name or list of molecule names
+            :param step: floats with the lr size for the optimization
+            :param maxiter: int maximum number of iterations
+            :param miniter: int minimum number of iterations
+            :param method: str method for optimization
+            :param diverge: float diverge value
+            :param maxdivattempts: int maximum number of divergence attempts
+            :param output_path: str path to output folder
+            :param get_misc: bool if True the misc dictionary is returned
+            :param mol_kwargs: dict kwargs for Molecule class
+            :param minimize_kwargs: dict kwargs for minimize function
+            :param out_kwargs: dict kwargs for save_output function
+            :return:
+        """
+        self.basis = basis
+        self.basis_ref = basis_ref
+        self.molecule = molecule
+        self.step = step
+        self.maxiter = maxiter
+        self.miniter = miniter
+        self.method = method
+        self.diverge = diverge
+        self.maxdivattempts = maxdivattempts
+        self.output_path = output_path
+        self.get_misc = get_misc
+        self.mol_kwargs = mol_kwargs
+        self.minimize_kwargs = minimize_kwargs
+        self.out_kwargs = out_kwargs
+
+        self.atomstruc = loadatomstruc(self.molecule, **self.mol_kwargs).atomstruc
+        self.opt_bparams = torch.Tensor
+        self.misc_params = dict
+
+
+    def select_optimal_optimizer(self):
+        print("selecting optimal optimizer")
+        if isinstance(self.molecule, str) and isinstance(self.step, float):
+            print("optimizing basis for single molecule")
+            return self.optimize_basis_single_molecule
+
+        elif isinstance(self.molecule, list) and isinstance(self.step, float):
+            check_atomstruclist_of_str = all(
+                    isinstance(elem, str) for elem in self.molecule)  # chek if list of atomstruc is list of molecule names
+            if check_atomstruclist_of_str:
+                print("optimizing basis for list of molecules")
+                return self.optimize_basis_list_of_molecules
+            else:
+                print("optimizing basis for a molecule but input is not a"
+                      " list of molecule names instead a list of atomstruc")
+                return self.optimize_basis_single_molecule
+
+        elif isinstance(self.molecule, str) and isinstance(self.step, list):
+            print("optimizing basis for single molecule but input is a list of steps")
+            return self.optimize_basis_single_molecule_list_of_steps
+        elif isinstance(self.molecule, list) and isinstance(self.step, list):
+            print("optimizing basis for list of molecules but input is a list of steps")
+            return self.optimize_basis_list_of_molecules_list_of_steps
+        else:
+            raise ValueError("input is not valid")
+
+    def _convert_basis(self, opt_bparams, bpacker):
+        """
+        convert Torch.tensor to scf  basis
+        :return: list
+        """
+        opt_basis = bconv(opt_bparams, bpacker)
+        return opt_basis
+
+    def calc_optbasis_energy(self, basis,atomstruc, outpath: str = "./"):
+        """
+        runs a dft calculation using pyscf.
+        :return: total energy of the system.
+        """
+
+        for val in basis.values():
+            for element in val:
+                for orb in element:
+                    if type(orb) is list:
+                        if any(isnan(orb)):
+                            warnings.warn("Your Basis includes NaN Values no energy calculated")
+                            return nan
+
+        mol = gto.Mole()
+        mol.atom = self.atomstruc
+        mol.unit = 'Bohr'  # in Angstrom
+        mol.verbose = 6
+        if self.molecule is not None:
+            if os.path.exists("./output"):
+                # try to save it in outputfolder
+                savep = f'./output/scf_optB_{self.molecule}.out'
+
+                exist = os.path.exists(savep)
+                if exist:
+                    it = 1
+                    while exist:  # if you run multipile calc take care to not overwrite the old output files
+
+                        exist = os.path.exists(f"{outpath}/scf_optB_{self.molecule}_00{it}.out") \
+                                or os.path.exists(f"{outpath}/scf_optB_{self.molecule}_0{it}.out") \
+                                or os.path.exists(f"{outpath}/scf_optB_{self.molecule}_{it}.out")
+                        if exist:
+                            it += 1
+                        else:
+                            if it < 10:
+                                mol.output = f"{outpath}/scf_optB_{self.molecule}_00{it}.out"
+                            elif it >= 10 and it < 100:
+                                mol.output = f"{outpath}/scf_optB_{self.molecule}_0{it}.out"
+                            else:
+                                mol.output = f"{outpath}/scf_optB_{self.molecule}_{it}.out"
+
+                else:
+                    mol.output = savep
+            else:
+                mol.output = f'scf_optB_{self.molecule}.out'  # save in home folder
+        else:
+            mol.output = "scf.out"
+
+        mol.symmetry = False
+        mol.basis = basis
+
+        try:
+            mol.spin = 0
+            mol.build()
+        except:
+            mol.spin = 1
+            mol.build()
+
+        mf = scf.RKS(mol)
+        mf.xc = 'B3LYP'
+        mf.kernel()
+        return mf.energy_tot()
+
+    def _setup_output_path(self):
+        """
+        setup output path and writer path for tensorboard and output files
+        :return: str path to output folder and str path to tensorboard folder
+        """
+        return conf_output(self.basis, self.basis_ref, self.molecule, outf=self.output_path, **self.out_kwargs)
+
+    def _get_mol_setup(self):
+        """
+        setup molecule object and dictionary of kwargs for minimized function
+        :return: Molecule object of each basis and dict of args for function that gets minimized
+
+        """
+        bsys1, bsys2, func_dict = Mole_minimizer(self.basis, self.basis_ref, self.molecule, **self.mol_kwargs)
+        bsys1.get_SCF(atomstrucstr=self.molecule)
+        energy_small_basis = bsys1.SCF.get_tot_energy
+        energy_ref_basis = bsys2.SCF.get_tot_energy
+
+        return energy_small_basis, energy_ref_basis, func_dict
+
+    def _save_out(self, energy_small_basis, energy_ref_basis, func_dict, outpath):
+        optbasis = bconv(self.opt_bparams, func_dict["bpacker"])
+        optbasis_energy = self.calc_optbasis_energy(optbasis, self.molecule, outpath)
+
+        save_output(outpath, self.basis, energy_small_basis, self.basis_ref, energy_ref_basis, optbasis,
+                    optbasis_energy,
+                    self.molecule, self.step, self.maxiter, miniter=self.miniter, method=self.method,
+                    packer=func_dict["bpacker"], misc=self.misc, optkwargs=self.minimize_kwargs)
+        return self
+
+    def run(self, func_dict, writer = None):
+        """
+        run the optimization
+        :return: torch.Tensor of optimized basis and dict of misc values
+        """
+        min_bparams =  xitorch.optimize.minimize(projection,
+                                        func_dict["bparams"],
+                                        (func_dict["bpacker"],
+                                         func_dict["ref_basis"],
+                                         func_dict["atomstruc_dqc"],
+                                         func_dict["atomstruc"],
+                                         func_dict["coeffM"],
+                                         func_dict["occ_scf"],
+                                         func_dict["num_gauss"],),
+                                        step=self.step,
+                                        method=self.method,
+                                        maxiter=self.maxiter,
+                                        miniter=self.miniter,
+                                        verbose=True,
+                                        writer=writer,
+                                        diverge=self.diverge,
+                                        maxdivattempts=self.maxdivattempts,
+                                        get_misc=self.get_misc,
+                                        **self.minimize_kwargs)
+        if self.get_misc:
+            self.opt_bparams = min_bparams[0]
+            self.misc = min_bparams[1]
+            return min_bparams[0], min_bparams[1]
+        else:
+            self.opt_bparams = min_bparams
+            return min_bparams
+
+
+    def optimize_basis_single_molecule(self, save_out=True):
+        energy_small_basis, energy_ref_basis, func_dict = self._get_mol_setup()
+
+        if save_out:
+            writerpath, outpath = self._setup_output_path()
+
+            writer = SummaryWriter(writerpath)
+        else:
+            writer = None
+            outpath = None
+
+        self.run(func_dict, writer=writer)
+
+        self._save_out(energy_small_basis, energy_ref_basis, func_dict, outpath)
+        return self
+
+
+
+
+
+
+    def __repr__(self):
+        return f"OPTBASIS(basis={self.basis}, basis_ref={self.basis_ref}, molecule={self.molecule}, step={self.step}," \
+               f" maxiter={self.maxiter}, miniter={self.miniter}, method={self.method}, diverge={self.diverge}," \
+               f" maxdivattempts={self.maxdivattempts}, output_path={self.output_path}, get_misc={self.get_misc}," \
+               f" mol_kwargs={self.mol_kwargs}, minimize_kwargs={self.minimize_kwargs}, out_kwargs={self.out_kwargs})"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def optimize_basis(basis: str,
                    basis_ref : str,
                    atomstruc : Union[str, list],
@@ -387,7 +645,8 @@ def optimize_basis(basis: str,
                    minimize_kwargs: dict = {},
                    out_kwargs : dict = {}):
     """
-    function to optimize basis functions.
+    function to optimize basis functions. It uses the xitorch.optimize.minimize function.
+    be aware that the basis functions are optimized in the small basis set.
     :param basis: str basis which should be optimized
     :param basis_ref: str reference basis
     :param atomstruc: Molecule Structure
@@ -455,8 +714,8 @@ def optimize_basis(basis: str,
                                                       maxdivattempts=maxdivattempts,
                                                       get_misc=get_misc,
                                                       **minimize_kwargs)
-        misc = {}
 
+    print(min_bparams)
     bsys1.get_SCF(atomstrucstr=atomstruc)
     energy_small_basis = bsys1.SCF.get_tot_energy
     energy_ref_basis = bsys2.SCF.get_tot_energy
@@ -485,6 +744,7 @@ def opt_basis_mult_steps(basis: str,
                    out_kwargs : dict = {}):
     """
     Optimize basis for multiple learning rates.
+    Be aware that this function done in optimization for each learning rate in serial.
     """
     for i in step:
         optimize_basis(basis, basis_ref, atomstruc, i, maxiter, miniter, method,
@@ -507,6 +767,7 @@ def mult_atomstruc(basis: str,
                    out_kwargs : dict = {}):
     """
     Optimize basis for multiple atom structures
+    Be aware that this function done in optimization for each atom structure in serial.
     """
     for i in atomstruc:
         optimize_basis(basis, basis_ref, i, step, maxiter, miniter, method,
