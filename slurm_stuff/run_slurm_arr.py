9c7e43541f0df449ba910753484cf6d9c23cbf48
jaikinator <schmieder.jacobweb.de>
2022-06-07 14:53:58 +0200

    working now

--- slurm_stuff/run_slurm_arr.py
+++ slurm_stuff/run_slurm_arr.py
@@ -13,7 +13,7 @@ from time import localtime
 ########################################################################################################################
 startdate = localtime()
 executable = sys.executable
-shrunf = "run_slurm.sh"
+shrunf = "run_slurm_OPTB.sh"
 slurm_outfolder = f"slurm_out/{startdate.tm_year}_{startdate.tm_mon}_{startdate.tm_mday}"
 job_name = "OPTB"
 mail = "jacob.schmieder@student.uni-halle.de" # mail address for slurm
@@ -23,23 +23,34 @@ conda_env = "OptBasis"
 gcc_version = "gcc/7.3.0"
 
 input_file_name = "training_input.json"
-execute_file_name = "main_slurm_arr.py"
+execute_file_name = "test_run.py"
 ########################################################################################################################
 # setup resources
 ########################################################################################################################
 runtime = "0-00:01"  # days-hours:minutes:seconds
-cpu_per_task = 8 # number of cpu per task
-mem_per_task = "16G" # memory per task
+cpu_cores_per_task = 2 # number of cpu per task
+mem_per_task = "111M" # memory per task
+queue = "standard" # queue name
+
+########################################################################################################################
+# execute through this script
+########################################################################################################################
+execute_file = True
 
 
+########################################################################################################################
+# create output folder structure if not exist
+########################################################################################################################
 
-#create output folder structure if it does not exist
 if not os.path.exists(slurm_outfolder):
     os.makedirs(slurm_outfolder)
 
 print(f"folder created {slurm_outfolder}")
 
-mol = w417
+########################################################################################################################
+# create input file
+########################################################################################################################
+mol = w417[1]
 learning_rate = [2e-2, 2e-3, 2e-4, 2e-5, 2e-6, 2e-7, 2e-8, 2e-9, 2e-10]
 basis = ["STO-3G,3-21G", "3-21G,cc-pvtz", "cc-pvdz,cc-pvtz", "aug-cc-pVDZ,aug-pc-2"]
 method = ["gd", "adam"]
@@ -63,19 +74,34 @@ in_dict =  {"molecule": mol,
 in_dict = create_dict(**in_dict)
 save_to_file(in_dict, input_file_name)
 
-with open("test_slurm.sh", "w") as f:
+########################################################################################################################
+# create slurm file
+########################################################################################################################
+
+with open(shrunf , "w") as f:
     f.write("#!/bin/bash\n")
+
     f.write(f"#SBATCH --job-name={job_name} \n")
     f.write(f"#SBATCH -t {runtime} \n")
-    f.write("#SBATCH -n 1   \n")
+    f.write(f"#SBATCH -n {cpu_cores_per_task}   \n")
     f.write(f"#SBATCH --mem={mem_per_task} \n")
+    f.write(f"SBATCH -p {queue}\n")
+
+    if exclude:
+        f.write(f"#SBATCH --exclude={exclude}\n")
+
+
     f.write("#SBATCH --mail-type=START,END,FAIL   # notifications for job done & fail \n")
     f.write(f"#SBATCH --mail-user={mail} \n")
-    f.write(f"#SBATCH -x {exclude} \n")
+
+
+    f.write(f"\n")
 
     f.write(f"ml {conda_version} \n")
     f.write(f"ml {gcc_version} \n")
     f.write(f"source activate {conda_env}\n")
+
+    f.write(f"\n")
 
     f.write(f"#SBATCH --output={slurm_outfolder}/slurm_output_%A_%a.out \n")
 
@@ -83,5 +109,11 @@ with open("test_slurm.sh", "w") as f:
 
     f.write(f"{executable} {execute_file_name} -f {input_file_name} -i $SLURM_ARRAY_TASK_ID")
 
-os.system(f"sbatch --array 0-{len(in_dict) -1} run_slurm.sh")
+
+########################################################################################################################
+# execute slurm file
+########################################################################################################################
+if execute_file:
+    os.system(f"sbatch --array 0-{len(in_dict) -1} {shrunf}")
+
 
