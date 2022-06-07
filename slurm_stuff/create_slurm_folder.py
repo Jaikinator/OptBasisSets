3f76501a7ea6e4119b86ed0a289b69f1727b19d5
jaikinator <schmieder.jacobweb.de>
2022-06-07 11:08:28 +0200

    removed only run as main

--- slurm_stuff/create_slurm_folder.py
+++ slurm_stuff/create_slurm_folder.py
@@ -1,6 +1,7 @@
 import os
 import sys
 
-if __name__ == "__main__":
-    if not os.path.exists(sys.argv[1]):
-        os.mkdir(sys.argv[1])
+
+if not os.path.exists(sys.argv[1]):
+    os.mkdir(sys.argv[1])
+
